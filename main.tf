e0f39bf3f2cd8f8362d7b2522289f92fcf4b846c
discocorg <david.flaten@gmail.com>
2023-11-12 15:48:03 -0600

    Minor adjustments to use configured aws credentials when deploying.

--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
 
 provider "aws" {
-    region = "us-east-1"
+  region                   = "us-east-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile = "admin"
 }
 
 resource "aws_s3_bucket" "first_bucket" {
-    bucket = "first-bucket-david"
+  bucket = "first-bucket-b1"
 }
