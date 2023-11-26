005fa9831c87887a97be3356c8072da429aad020
discocorg <david.flaten@gmail.com>
2023-11-26 15:42:44 -0600

    Adding explanation comments to terraform definition.

--- main.tf
+++ main.tf
@@ -16,6 +16,11 @@ provider "aws" {
   profile = "admin"
 }
 
+# Any resource that starts with `aws_` tells you it will be from AWS
+# if you had `google_` instead it would be from GCP.
+# if you had `poastgresql_` it would be from Postgres
 resource "aws_s3_bucket" "first_bucket" {
+  # Some properties on resources are optional and some are required.
+  # Bucket for an S3 bucket defines the name of the bucket which is required.
   bucket = "first-bucket-b1"
 }
